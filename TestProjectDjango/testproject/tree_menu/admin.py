47dd63ad543196f39a9698f5c8a76fbd3f8dd1d9
daniilsapunov <145489531+daniilsapunov@users.noreply.github.com>
2025-11-13 17:36:30 +0300

    Delete TestProjectDjango/testproject directory

--- TestProjectDjango/testproject/tree_menu/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem
-
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'menu_name', 'parent', 'order', 'url')
-    list_filter = ('menu_name',)
-    search_fields = ('name', 'url')
-    raw_id_fields = ('parent',)
--- TestProjectDjango/testproject/tree_menu/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TreeMenuConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tree_menu'
